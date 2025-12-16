0943740093e2d803770a8128a533ef222c6e0190
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-16 21:53:06 +0400

    lesson 1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div className="App-wrapper">
+        <header className="app-header">
+            <img src={logo}
+                 className="app-logo"
+                 alt="logo"/>
+        </header>
+        <nav className="app-nav">
+            <div><a href="">Profile</a></div>
+            <div><a href="">News</a></div>
+            <div><a href="">Music</a></div>
+            <div><a href="">Settings</a></div>
+        </nav>
+        <main className="app-main">
+            <div className='main-poster'>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                    <div>post 3</div>
+                </div>
+            </div>
+        </main>
+    </div>);
 }
 
 export default App;

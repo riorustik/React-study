d174facbcc76801fa9d847c5681422d5e50a62b9
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-17 17:20:22 +0400

    lesson 6

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import {Header} from "./Components/Header/Header";
 import {NavBar} from "./Components/NavBar/NavBar";
 import {Profile} from "./Components/pages/Profile/Profile";
 import {Messages} from "./Components/pages/Messages/Messages";
-
+import {BrowserRouter, Route, Routes} from "react-router";
 
 function App() {
-    return (<div className="App-wrapper">
-        <Header/>
-        <NavBar/>
-        <main className='App-content'>
-            {/*<Profile/>*/}
-            <Messages/>
-        </main>
-    </div>);
+    return (
+        <BrowserRouter>
+            <div className="App-wrapper">
+                <Header/>
+                <NavBar/>
+                <main className='App-content'>
+                    <Routes>
+                        <Route path='Profile'
+                               element={<Profile />}/>
+                        <Route path='Messages'
+                               element={<Messages />}/>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>);
 }
 
 export default App;
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,15 +4,15 @@ import styles from "./NavBar.module.css";
 export function NavBar() {
     return (
         <nav className={styles.nav}>
-            <div><a href="#"
+            <div><a href="/Profile"
                     className={styles.nav__link}>Profile</a></div>
-            <div><a href="#"
+            <div><a href="Messages"
                     className={styles.nav__link}>Messages</a></div>
-            <div><a href="#"
+            <div><a href="News"
                     className={styles.nav__link}>News</a></div>
-            <div><a href="#"
+            <div><a href="Music"
                     className={styles.nav__link}>Music</a></div>
-            <div><a href="#"
+            <div><a href="Settings"
                     className={styles.nav__link}>Settings</a></div>
         </nav>
     )

864df2445bb7d4ea2ef4f7a63419ae0ad02fa68c
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-17 17:59:15 +0400

    lesson 7

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import styles from "./NavBar.module.css";
+import {Link, NavLink} from "react-router";
 
 export function NavBar() {
+    // const links = document.querySelectorAll('.navLink'); //ИЗМЕНЕНИЕ АКТИВНОЙ ССЫЛКИ
+    //
+    // links.forEach(link => {
+    //     link.addEventListener('click', (e) => {
+    //         e.target.classList.add('.navLinkActive');
+    //         console.log('11')
+    //     })
+    // })
+
     return (
         <nav className={styles.nav}>
-            <div><a href="/Profile"
-                    className={styles.nav__link}>Profile</a></div>
-            <div><a href="Messages"
-                    className={styles.nav__link}>Messages</a></div>
-            <div><a href="News"
-                    className={styles.nav__link}>News</a></div>
-            <div><a href="Music"
-                    className={styles.nav__link}>Music</a></div>
-            <div><a href="Settings"
-                    className={styles.nav__link}>Settings</a></div>
+            <div><Link to="/Profile"   //navlink obsolete
+                    className={`${styles.navLink}`}>Profile</Link></div>
+            <div><Link to="Messages"
+                    className={styles.navLink}>Messages</Link></div>
+            <div><Link to="News"
+                    className={styles.navLink}>News</Link></div>
+            <div><Link to="Music"
+                    className={styles.navLink}>Music</Link></div>
+            <div><Link to="Settings"
+                    className={styles.navLink}>Settings</Link></div>
         </nav>
     )
 }

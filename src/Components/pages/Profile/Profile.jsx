681e58f8b2305dbe720733185f6e9d2585b1f0a8
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-20 23:02:59 +0400

    lesson 10

--- src/Components/pages/Profile/Profile.jsx
+++ src/Components/pages/Profile/Profile.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import style from "./Profile.module.css";
 import {AddPost} from "./AddPost/AddPost";
+import {UserPersonalInfo} from "./UserPersonalInfo/UserPersonalInfo";
 
 export function Profile() {
     return (
         <section className={style.profile}>
-            <div    >
-                ava + description
-            </div>
+            <UserPersonalInfo />
             <AddPost/>
         </section>
     )

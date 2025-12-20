7488f12014798212fe8cc278b719cfe8bbc6a00d
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-20 23:26:16 +0400

    new style for navigation block

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import styles from "./NavBar.module.css";
-import {Link, NavLink} from "react-router";
+import {Link} from "react-router";
 
 export function NavBar() {
-    // const links = document.querySelectorAll('.navLink'); //ИЗМЕНЕНИЕ АКТИВНОЙ ССЫЛКИ
-    //
-    // links.forEach(link => {
-    //     link.addEventListener('click', (e) => {
-    //         e.target.classList.add('.navLinkActive');
-    //         console.log('11')
-    //     })
-    // })
-
     return (
         <nav className={styles.nav}>
-            <div><Link to="/Profile"   //navlink obsolete
-                    className={`${styles.navLink}`}>Profile</Link></div>
-            <div><Link to="/Chats"
-                    className={styles.navLink}>Chats</Link></div>
-            <div><Link to="News"
-                    className={styles.navLink}>News</Link></div>
-            <div><Link to="Music"
-                    className={styles.navLink}>Music</Link></div>
-            <div><Link to="Settings"
-                    className={styles.navLink}>Settings</Link></div>
+            <Link to="/Profile"
+                  className={`${styles.navLink}`}><i className="fa-solid fa-house"></i></Link>
+            <Link to="/Chats"
+                  className={styles.navLink}><i className="fa-solid fa-comments"></i></Link>
+            <Link to="News"
+                  className={styles.navLink}><i className="fa-solid fa-newspaper"></i></Link>
+            <Link to="Music"
+                  className={styles.navLink}><i class="fa-solid fa-headphones"></i></Link>
+            <Link to="Settings"
+                  className={styles.navLink}><i className="fa-solid fa-gear"></i></Link>
         </nav>
     )
 }
+
+
+// <div className="liquid-bar">
+//     <div className="liquid-item">
+//         M
+//     </div>
+//     <div className="liquid-item">
+//         S
+//     </div>
+//     <div className="liquid-item">
+//         R
+//     </div>
+// </div>

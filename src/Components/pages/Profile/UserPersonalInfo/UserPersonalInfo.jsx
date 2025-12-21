7b27f7bfa54504790d512f88af9e103eaa907785
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-21 18:20:28 +0400

    ava/description/link

--- src/Components/pages/Profile/UserPersonalInfo/UserPersonalInfo.jsx
+++ src/Components/pages/Profile/UserPersonalInfo/UserPersonalInfo.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import style from "./UserPersonalInfo.module.css";
+import ava from '../../../../image/ava.jpg'
+import {Link} from "react-router";
 
 export function UserPersonalInfo() {
     return (
-        <div>
-            ava + description
+        <div className={style.profileInfo}>
+            <img src={ava}
+                 className={style.profileAvatar}
+                 alt=""/>
+            <div className={style.profileDescription}>
+                <h1><span>Кусяпкулов </span> <br/> Рустам </h1>
+                <br/>
+                <p>Фронтенд разработчик, в поисках работы демонстриющий свои навыки прямо сейчас пока вы читаете этот
+                    текст</p>
+
+                <Link to='/Profile/Posts' className={style.button}> Создать запись</Link>
+            </div>
         </div>
     )
 }

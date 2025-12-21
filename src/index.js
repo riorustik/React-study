e4046390b5a712ecb7b6ee4649eb8bf1f63b9922
Rustam Kusiapkulov <riorustik@outlook.com>
2025-12-21 23:49:00 +0400

    fix style&prettier

--- src/index.js
+++ src/index.js
@@ -35,10 +35,11 @@ let postsPageData = {postsData: postsData};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App
-        chatsPageData={chatsPageData} postsPageData={postsPageData}/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App
+            chatsPageData={chatsPageData}
+            postsPageData={postsPageData}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
